04150e9afeaa5e012abf8135fdd4ad80f8525ff7
Kina Rain <tina@lawtk.net>
2020-03-26 11:51:58 -0400

    heroku deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const path = require('path');
 const apiRoutes = require('./API');
 
 //API Route Setup
 router.use('/api', apiRoutes);
+
+//setup the path to the react world
+let staticPath = path.join(__dirname, '../../client/public');
+
+console.log(staticPath);
+
+// Handle React routing, return all requests to React app
+router.use((req, res) => {
+    res.sendFile('index.html', { root: staticPath });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,8 +4,6 @@ if (process.env.NODE_ENV != 'production') { const dotenv = require('dotenv/confi
 const routes = require('./routes');
 const path = require('path');
 
-console.log(process.env.NODE_ENV);
-
 //setup our port
 let PORT = process.env.PORT;
 
@@ -16,17 +14,9 @@ let app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-
-    let staticPath = path.join(__dirname, '../client/build');
-    // Serve any static files
-    app.use(express.static(staticPath));
-
-    // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
-        res.sendFile('index.html', { staticPath });
-    });
-}
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../client"));
+  }
 
 //use the routes
 app.use(routes);
