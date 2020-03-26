bb0431679991befa507e0accadda72521196a76b
Kina Rain <tina@lawtk.net>
2020-03-26 08:50:33 -0400

    trying for heroku render

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-if(process.env.NODE_ENV != 'production') {const dotenv = require('dotenv/config')};
+if (process.env.NODE_ENV != 'production') { const dotenv = require('dotenv/config') };
 //const routes = require('./src/routes');
 const path = require('path');
 
@@ -13,23 +13,25 @@ let PORT = process.env.PORT;
 let app = express();
 
 //define the middle ware stuff
-app.use(express.urlencoded( { extended: true}) );
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') { 
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, '../client')));  
+if (process.env.NODE_ENV === 'production') {
 
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client','build','index.html'));
-  }); 
+    let staticPath = path.join(__dirname, '../client/build');
+    // Serve any static files
+    app.use(express.static(staticPath));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile('index.html', { staticPath });
+    });
 }
 
 //use the routes
 //app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.listen(PORT, () => {
     console.log(`Web Running ==> Listening on Port: ${PORT}`);
